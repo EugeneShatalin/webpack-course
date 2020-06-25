9163529ff85c6b70240ee39bacd6d66243e6538a
EugeneShatalin <shatalin_ei@mail.ru>
2020-06-25 18:13:51 +0600

    Подключил к проекту JQuery, сделал обновление файлов проекта без перезапуска сборки добавив флаг --watch.

--- src/modules/header.component.js
+++ src/modules/header.component.js
@@ -1,1 +1,9 @@
-console.log('Header componentnpm ')
+import $ from 'jquery' //импортируем библиотеку jquery в переменную $
+
+$('<h1 />')//создаем элемент с помощью библиотеки
+    .text('Hello world from JQuery') //вставляем текст в элемент
+    .css({ //создаем стили
+        textAlign: 'center',
+        color: 'blue'
+    })
+    .appendTo($('header')) //элемент импортируем в тег header
